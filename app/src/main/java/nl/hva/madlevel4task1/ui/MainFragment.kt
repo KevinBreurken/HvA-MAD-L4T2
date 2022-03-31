a24211e51324a7bbffb7b5cd90be7a9b4d8c0e64
Kevin Breurken <kevinbreurken@gmail.com>
2022-03-31 21:44:51 +0200

    Fragments are rendered in the activity

--- app/src/main/java/nl/hva/madlevel4task1/ui/MainFragment.kt
+++ app/src/main/java/nl/hva/madlevel4task1/ui/MainFragment.kt
@@ -1,6 +1,22 @@
 package nl.hva.madlevel4task1.ui
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import nl.hva.madlevel4task1.databinding.FragmentMainBinding
 
 class MainFragment: Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
 }
